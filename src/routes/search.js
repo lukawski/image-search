4e5db9acb099f561050255646b4c0852b21d56fe
Kacper Łukawski <ka.lukawski@gmail.com>
2017-03-02 22:57:42 +0100

    Scrapper is working!

--- src/routes/search.js
+++ src/routes/search.js
@@ -8,9 +8,16 @@ router.get('/:searchString', (req, res) => {
   request(`https://www.google.pl/search?q=${req.params.searchString}&source=lnms&tbm=isch`, (err, response, body) => {
     if (err) console.log(err)
     var $ = cheerio.load(body)
+    console.log($('.images_table').children().text())
+    $('.images_table').children().each(function (i, item) {
+      $(this).children('td').each(function (i, item) {
+        console.log($(this).children('a').children('img').attr('src'))
+      })
+      console.log('elo')
+    })
+    res.send(body)
   })
-  console.log(req.params.searchString)
-  res.send('elo')
+  //res.send('elo')
 })
 
 module.exports = router
