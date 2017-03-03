0f6c639970748c84cbac1109e72a940d207f03a4
Kacper Łukawski <ka.lukawski@gmail.com>
2017-03-03 20:55:18 +0100

    Heroku db uri

--- src/server.js
+++ src/server.js
@@ -4,10 +4,11 @@ import routes from './routes/search'
 
 const app = express()
 const port = process.env.PORT || 3000
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/searches'
 
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost/searches')
+mongoose.connect(dbUri)
   .then(() => console.log('Database connected'))
   .catch((err) => console.log(err))
 
